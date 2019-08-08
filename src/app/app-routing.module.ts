c142ab8419a61126e4da14964b69406f1674ec35
NishaRaut <nisharaut@gmail.com>
2019-08-08 16:52:04 +0530

    tech-requirement component added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TechRequirementComponent } from './component/tech-requirement/tech-requirement.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'techreq', component: TechRequirementComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './component/login/login.component';
 import { RegisterComponent } from './component/register/register.component';
+import { TechRequirementComponent } from './component/tech-requirement/tech-requirement.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    TechRequirementComponent
   ],
   imports: [
     BrowserModule,
