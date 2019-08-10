30277081dbc500807761136fde79145612c4f2e0
Rohan2596 <rohankadam662>
2019-08-10 13:20:59 +0530

    Forgot And resetPassword

--- src/app/component/reset-password/reset-password.component.ts
+++ src/app/component/reset-password/reset-password.component.ts
@@ -17,25 +17,23 @@ export class ResetPasswordComponent implements OnInit {
   hide1: boolean = false;
   disableSubmit: boolean = false;
   resetForm: FormGroup;
-  password = new FormControl(resetDetails.password, [Validators.required, Validators.minLength(5)]);
-  confirmpassword=new FormControl(resetDetails.confirmpassword,[Validators.required,Validators.minLength(5)]);
+  password = new FormControl(resetDetails.password, [Validators.required, Validators.minLength(5), Validators.maxLength(8)]);
+  confirmpassword=new FormControl(resetDetails.confirmpassword,[Validators.required, Validators.minLength(5), Validators.maxLength(8)]);
   constructor(private router: Router, ) { }
 
   ngOnInit() {
   }
 
-  getErrorMessagePassword() {
+  getErrorMessageConfrimPassword() {
     return this.confirmpassword.hasError('required') ? 'Password Feild Is Required.' :
       this.confirmpassword.hasError('minLength') ? 'Min Character For Password is 5' :
+      this.confirmpassword.hasError('maxLength') ? 'Max Character For Password is 8' :
         '';
   }
-  getErrorMessageConfrimPassword() {
+  getErrorMessagePassword() {
     return this.password.hasError('required') ? 'Password Feild Is Required.' :
       this.password.hasError('minLength') ? 'Min Character For Password is 5' :
-        '';
-  }
-  reset(){
-console.log('reset');
+      this.password.hasError('maxLength') ? 'Max Character For PassWord is 8' : '';
   }
   login(){
     this.router.navigate(['/login'])
