114bba31ad9371b1d4b808e23b7437b1d10384db
NishaRaut <nisharaut@gmail.com>
2019-08-16 15:06:08 +0530

    input css part done

--- src/app/component/veiw-sheets/veiw-sheets.component.ts
+++ src/app/component/veiw-sheets/veiw-sheets.component.ts
@@ -7,8 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VeiwSheetsComponent implements OnInit {
  
-  headers = [0,1,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18];
-  rows = [0,1,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18];
+  headers = [5,8,9,5,8,9,5,8,9,5,8,9,5,8,9,5,8,9];
+  rows = [5,8,9,5,8,9,5,8,9,5,8,9,5,8,9,5,8,9];
   constructor() { }
   arrayLength=0;
   num : any=[];
